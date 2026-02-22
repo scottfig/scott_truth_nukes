f3be4254532466618da612f5bc2d4e42eaa3847d
Scott Figueroa <scottricardo@ucsb.edu>
2026-02-21 19:02:06 -0800

    added google compliance

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: "Scott's Truth Nukes - Insightful PDFs",
   description: 'Discover powerful insights into the world with Scott\'s Truth Nukes PDF collection',
+  verification: {
+    google: 'G7Ptldk4QQ8Ylc_o8A0drzjnB4ePGTY-LxRbXQWlzSA',
+  },
 }
 
 export default function RootLayout({
